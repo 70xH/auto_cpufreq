4d232a186bb11556b37729efda17c22f5c5733fa
70xH <70xH@tutanota.com>
2020-11-20 22:29:33 +0000

    mostly no changes

---
+++ scripts/auto-cpufreq-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# auto-cpufreq daemon install script
+# reference: https://github.com/AdnanHodzic/auto-cpufreq
+
+echo -e "\n------------------ Running auto-cpufreq daemon install script ------------------"
+
+if [[ $EUID != 0 ]]; 
+then
+	echo -e "\nERROR\nMust be run as root (i.e: 'sudo $0')\n"
+	exit 1
+fi
+
+echo -e "\n* Deploy auto-cpufreq systemd unit file"
+mkdir /etc/sv/auto-cpufreq
+cp /usr/local/share/auto-cpufreq/scripts/run /etc/sv/auto-cpufreq
+chmod +x /etc/sv/auto-cpufreq/run
+
+echo -e "\n* Initiating the service"
+ln -s /etc/sv/auto-cpufreq /var/service
+
+echo -e "\n* Stopping auto-cpufreq daemon service"
+sv stop auto-cpufreq
+
+echo -e "\n* Starting auto-cpufreq daemon service"
+sv start auto-cpufreq
+
+echo -e "\n* Enabling auto-cpufreq daemon service at boot"
+sv enable auto-cpufreq
---
+++ scripts/auto-cpufreq-remove.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# auto-cpufreq daemon removal script
+# reference: https://github.com/AdnanHodzic/auto-cpufreq
+
+echo -e "\n------------------ Running auto-cpufreq daemon removal script ------------------"
+
+if [[ $EUID != 0 ]]; 
+then
+	echo -e "\nERROR\nMust be run as root (i.e: 'sudo $0')\n"
+	exit 1
+fi
+
+echo -e "\n* Stopping auto-cpufreq daemon (systemd) service"
+systemctl stop auto-cpufreq
+
+echo -e "\n* Disabling auto-cpufreq daemon (systemd) at boot"
+systemctl disable auto-cpufreq
+
+echo -e "\n* Removing auto-cpufreq daemon (systemd) unit file"
+rm /etc/systemd/system/auto-cpufreq.service
+
+echo -e "\n* Reloading systemd manager configuration"
+systemctl daemon-reload
+
+echo -e "reset failed"
+systemctl reset-failed
